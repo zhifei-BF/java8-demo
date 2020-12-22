a9902a03cb60488d3fa0576ee5a875b7b255e6f5
Liquor <helin.huang@estsh.com>
2020-12-22 22:41:48 +0800

    增加了lambda的练习

---
+++ src/main/java/com/liquor/java8/demo/exer/MyFunction2.java
@@ -0,0 +1,11 @@
+package com.liquor.java8.demo.exer;
+
+/**
+ * @Description:
+ * @Reference:
+ * @Author: Liquor
+ * @CreateDate:2020-12-22 22:29
+ * @Modify:
+ **/
+public class MyFunction2 {
+}
---
+++ src/main/java/com/liquor/java8/demo/exer/TestLambda.java
@@ -0,0 +1,11 @@
+package com.liquor.java8.demo.exer;
+
+/**
+ * @Description:
+ * @Reference:
+ * @Author: Liquor
+ * @CreateDate:2020-12-22 22:12
+ * @Modify:
+ **/
+public class TestLambda {
+}
