003cf702c61ec802408ae956c8d21ae887aaf8c9
Liquor <helin.huang@estsh.com>
2020-12-22 23:57:27 +0800

    增加方法引用的相关内容

---
+++ src/main/java/com/liquor/java8/demo/test/TestMethodRef.java
@@ -0,0 +1,11 @@
+package com.liquor.java8.demo.test;
+
+/**
+ * @Description:
+ * @Reference:
+ * @Author: Liquor
+ * @CreateDate:2020-12-22 23:21
+ * @Modify:
+ **/
+public class TestMethodRef {
+}
