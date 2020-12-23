6b16a68765e7bb11c7e4448c884c616c6df65dad
liquor.huang <liquor>
2020-12-23 20:01:26 +0800

    增加Stream流相关内容

--- src/main/java/com/liquor/java8/demo/test/TestStreamAPI.java
+++ src/main/java/com/liquor/java8/demo/test/TestStreamAPI.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -63,4 +64,61 @@ public class TestStreamAPI {
             new Employee(104, "赵六", 8, 7777.77),
             new Employee(105, "田七", 38, 5555.55)
     );
+
+    /*
+	  筛选与切片
+		filter——接收 Lambda ， 从流中排除某些元素。
+		limit——截断流，使其元素不超过给定数量。
+		skip(n) —— 跳过元素，返回一个扔掉了前 n 个元素的流。若流中元素不足 n 个，则返回一个空流。与 limit(n) 互补
+		distinct——筛选，通过流所生成元素的 hashCode() 和 equals() 去除重复元素
+	 */
+
+    //内部迭代：迭代操作Stream API内部完成
+    @Test
+    public void test2() {
+        //所有的中间操作不会做任何处理
+        Stream<Employee> stream = emps.stream()
+                .filter((e) -> {
+                    System.out.println("测试中间操作");
+                    return e.getAge() <= 35;
+                });
+        //只有当做终止操作时，所有的中间操作会一次性的全部执行，称为“惰性求值”
+        stream.forEach(System.out::println);
+    }
+
+    //外部迭代
+    @Test
+    public void test3() {
+        Iterator<Employee> it = emps.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+
+    @Test
+    public void test4() {
+        emps.stream()
+                .filter((e) -> {
+                    System.out.println("短路！");// && ||
+                    return e.getSalary() >= 5000;
+                })
+                .limit(3)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void test5() {
+        emps.stream()
+                .filter((e) -> e.getSalary() >= 5000)
+                .skip(2)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void test6() {
+        emps.stream()
+                .distinct()
+                .forEach(System.out::println);
+    }
+
 }
