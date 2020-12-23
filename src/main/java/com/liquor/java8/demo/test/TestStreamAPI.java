f48290dc323b73b7ce9a1212f4e63bfd02976291
liquor.huang <liquor>
2020-12-23 18:40:39 +0800

    测试Stream流上传是否成功

---
+++ src/main/java/com/liquor/java8/demo/test/TestStreamAPI.java
@@ -0,0 +1,11 @@
+package com.liquor.java8.demo.test;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author Liquor.Huang
+ * @Date 2020/12/23 17:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestStreamAPI {
+}
